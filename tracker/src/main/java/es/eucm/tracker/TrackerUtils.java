8376d851b9152e103079fb220b6b29712631031d
Ivan Martinez-Ortiz <imartinezortiz@gmail.com>
2020-01-31 21:05:21 +0100

    Updating license header

--- tracker/src/main/java/es/eucm/tracker/TrackerUtils.java
+++ tracker/src/main/java/es/eucm/tracker/TrackerUtils.java
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2016 e-UCM (http://www.e-ucm.es/)
+ * Copyright © 2019-20 e-UCM (http://www.e-ucm.es/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package es.eucm.tracker;
 
 import es.eucm.tracker.exceptions.KeyExtensionException;
